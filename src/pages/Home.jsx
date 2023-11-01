e71a552547141ed14d01c5336737c6b48d70734f
Uwancha <sito2alemu@gmail.com>
2023-11-01 23:38:23 +0300

    feat: Add Home page Create Home page with image and some texts Add Tailwind CSS class to style and layout the component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import image from "../assets/kal.png";
+
+const Home = () => {
+
+    
+    return (
+        <div className="w-full max-w-screen-md mx-auto flex flex-col items-center py-20">
+            
+            <img 
+                src={image} 
+                alt="kal's Icon"
+                className="w-40 rounded-full h-40 shadow-xl md:w-50 md:h-50 mb-20"
+            />
+
+            <div className="flex flex-col text-left">
+
+                <h4 className="text-xl font-bold mb-2">Hi there!</h4>
+    
+                <h3 className="text-gray-600 font-bold mb-2">I'm Kalkidan</h3>
+
+                <h3 className="text-gray-600 font-bold ">
+                    I'm a Video Editor.  
+                </h3>
+
+                <h3 className="w-30 text-gray-600 font-bold mb-5" >
+                    I create video content for brands, podcasts and influencers
+                </h3>
+                <a
+                    href="/about"
+                    className="mx-auto px-6 py-2 text-white bg-blue-600 rounded shadow-md hover:bg-blue-700"
+                >
+                    Learn More
+                </a>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
