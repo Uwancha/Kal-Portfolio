edc7b874ba92bab2e669335b49ece5c9d2c629cf
Uwancha <sito2alemu@gmail.com>
2023-11-15 22:51:20 +0300

    feat: Configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
 
 function App() {
   
 
   return (
     <div className='bg-white'>
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/projects' Component={Projects} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }
