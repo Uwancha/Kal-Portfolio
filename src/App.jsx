c4c0d744f3c1a8af6a8cefa9f442ea39322ce0c3
Uwancha <sito2alemu@gmail.com>
2023-11-17 15:36:17 +0300

    feat: Configure routing for 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Works from './pages/Projects'
+import NotFound from './pages/NotFound'
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' Component={Home} />
           <Route path='/projects' Component={Works} />
+          <Route path='*' Component={NotFound} />
         </Routes>
         <Footer />
       </Router>
