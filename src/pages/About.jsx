da695dd60587053019ef3577b833df25c8de6034
Uwancha <sito2alemu@gmail.com>
2023-11-23 16:23:29 +0300

    feat: Add About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='w-1/2 mx-auto mt-40'>
+        <section>
+            <h2 className='text-green-626 text-center text-3xl font-bold mb-8'>About me</h2>
+            <p className='text-slategray text-xl font-slight-bold'>
+                I am Kalkidan Birhanu, a passionate and skilled video editor with a deep love for storytelling through the art of visual media.
+                With two years of experience in the industry, I have had the privilege of working on a diverse range of projects,
+                including corporate videos, documentaries, short films, and promotional content.
+            </p>
+
+            <p className='text-slategray text-xl font-slight-bold mb-40 pt-10' >
+                I have interested in editing video then I started to work on this, Know I have experience in editing videos
+                through Adobe premiere pro, Wondershare Filmora, Microsoft Clipchamp, Davinci Resolve, Videos with powtoon, 
+                Final capcut and others.
+            </p>
+        </section>
+    </div>
+  )
+}
